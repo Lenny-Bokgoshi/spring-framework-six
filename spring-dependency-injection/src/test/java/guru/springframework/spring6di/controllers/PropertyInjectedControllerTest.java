4b967ab453c605c433252cb7815807627e247f5a
LennyBokgoshi <lenny@anylytical.co.za>
2024-03-16 19:07:51 +0200

    DI using Spring Framework

--- spring-dependency-injection/src/test/java/guru/springframework/spring6di/controllers/PropertyInjectedControllerTest.java
+++ spring-dependency-injection/src/test/java/guru/springframework/spring6di/controllers/PropertyInjectedControllerTest.java
@@ -3,20 +3,23 @@ package guru.springframework.spring6di.controllers;
 import guru.springframework.spring6di.services.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class PropertyInjectedControllerTest {
 
     // Not optimal to inject this way
+    @Autowired
     PropertyInjectedController propertyInjectedController;
 
-    @BeforeEach
-    void setUp() {
-
-        propertyInjectedController = new PropertyInjectedController();
-        propertyInjectedController.greetingService = new GreetingServiceImpl(); // NullPointer if forgotten
-    }
+//    @BeforeEach
+//    void setUp() {
+//        propertyInjectedController = new PropertyInjectedController();
+//        propertyInjectedController.greetingService = new GreetingServiceImpl(); // NullPointer if forgotten
+//    }
 
     @Test
     void sayHello() {
