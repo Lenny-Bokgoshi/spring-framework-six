6a7e5de1230d8ba155f666a6ae36b2d93a40650c
LennyBokgoshi <lenny@anylytical.co.za>
2024-03-17 08:46:49 +0200

    HTTP Get with Spring MVC List Operation

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -2,8 +2,11 @@ package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
     Beer getBeerById(UUID uuid);
+
+    List<Beer> listBeers();
 }
