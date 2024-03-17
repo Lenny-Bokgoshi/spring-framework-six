5d7da0c59471093bdd3d39828a3718961089cd2e
LennyBokgoshi <lenny@anylytical.co.za>
2024-03-17 15:02:56 +0200

    Assignment 5 - GET Customer

---
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controllers/CustomerController.java
@@ -0,0 +1,33 @@
+package guru.springframework.spring6restmvc.controllers;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.services.BeerService;
+import guru.springframework.spring6restmvc.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable("customerId") UUID customerId){
+        return customerService.getCustomerById(customerId);
+    }
+
+}
