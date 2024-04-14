dbade85c7dfa67bf2bbe24c14b650cfebdf8a199
LennyBokgoshi <lenny@anylytical.co.za>
2024-04-14 16:02:00 +0200

    Refactoring and additional tests

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controllers/CustomerController.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controllers/CustomerController.java
@@ -16,47 +16,52 @@ import java.util.UUID;
 public class CustomerController {
 
     public static final String CUSTOMER_PATH = "/api/v1/customer";
-    public static final String CUSTOMER_PATH_ID = "/api/v1/customer/{customerId}";
-
+    public static final String CUSTOMER_PATH_ID = CUSTOMER_PATH + "/{customerId}";
 
     private final CustomerService customerService;
 
     @PatchMapping(CUSTOMER_PATH_ID)
     public ResponseEntity patchCustomerById(@PathVariable("customerId") UUID customerId,
-                                            @RequestBody CustomerDTO customerDTO){
-        customerService.patchCustomerById(customerId, customerDTO);
+                                            @RequestBody CustomerDTO customer){
+
+        customerService.patchCustomerById(customerId, customer);
+
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping(CUSTOMER_PATH_ID)
     public ResponseEntity deleteCustomerById(@PathVariable("customerId") UUID customerId){
+
         customerService.deleteCustomerById(customerId);
+
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping(CUSTOMER_PATH_ID)
     public ResponseEntity updateCustomerByID(@PathVariable("customerId") UUID customerId,
-                                             @RequestBody CustomerDTO customerDTO){
-        customerService.updateCustomerById(customerId, customerDTO);
+                                             @RequestBody CustomerDTO customer){
+
+        customerService.updateCustomerById(customerId, customer);
+
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping(CUSTOMER_PATH)
-    public ResponseEntity handlePost(@RequestBody CustomerDTO customerDTO){
-        CustomerDTO savedCustomerDTO = customerService.saveNewCustomer(customerDTO);
+    public ResponseEntity handlePost(@RequestBody CustomerDTO customer){
+        CustomerDTO savedCustomer = customerService.saveNewCustomer(customer);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", "/api/v1/customer/" + savedCustomerDTO.getId().toString());
+        headers.add("Location", CUSTOMER_PATH + "/" + savedCustomer.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @GetMapping(CUSTOMER_PATH)
-    public List<CustomerDTO> listCustomers(){
+    public List<CustomerDTO> listAllCustomers(){
         return customerService.getAllCustomers();
     }
 
-    @GetMapping(CUSTOMER_PATH_ID)
+    @GetMapping(value = CUSTOMER_PATH_ID)
     public CustomerDTO getCustomerById(@PathVariable("customerId") UUID id){
         return customerService.getCustomerById(id).orElseThrow(NotFoundException::new);
     }
--- spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/controllers/CustomerControllerIT.java
+++ spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/controllers/CustomerControllerIT.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controllers;
 
+import guru.springframework.spring6restmvc.entities.Customer;
 import guru.springframework.spring6restmvc.model.CustomerDTO;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
@@ -9,33 +10,49 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class CustomerControllerIT {
 
     @Autowired
-    CustomerController customerController;
-
-    @Autowired
     CustomerRepository customerRepository;
 
-    @Test
-    void testListCustomers() {
-        List<CustomerDTO> dtos = customerController.listCustomers();
-
-        assertThat(dtos.size()).isEqualTo(3);
-    }
+    @Autowired
+    CustomerController customerController;
 
     @Rollback
     @Transactional
     @Test
-    void testEmptyList() {
+    void testListAllEmptyList() {
         customerRepository.deleteAll();
-        List<CustomerDTO> dtos = customerController.listCustomers();
+        List<CustomerDTO> dtos = customerController.listAllCustomers();
 
         assertThat(dtos.size()).isEqualTo(0);
+    }
+
+    @Test
+    void testListAll() {
+        List<CustomerDTO> dtos = customerController.listAllCustomers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Test
+    void testGetByIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            customerController.getCustomerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        Customer customer = customerRepository.findAll().get(0);
+        CustomerDTO customerDTO = customerController.getCustomerById(customer.getId());
+        assertThat(customerDTO).isNotNull();
     }
 
 }
